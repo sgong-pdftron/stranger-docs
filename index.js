5f8543279d52a876f4dc5c4d0663e5dee1c2642e
justinjung04 <justinjung04@gmail.com>
2017-11-29 10:48:43 -0800

    Moved rendering code from components to pages

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-import LandingPage from '../components/LandingPage';
-export default LandingPage;
+import React from 'react';
+import { withPrefix } from 'gatsby-link';
+
+import Link from '../components/Link';
+
+import './index.scss';
+
+export default () => (
+  <div className='pages-index'>
+    <div className='row'>
+      <div className='col'>
+        <h1><Link to='/webviewer'>WebViewer</Link></h1>
+        <p><Link to='/webviewer/guides'>Guides</Link></p>
+        <p><Link to='https://www.pdftron.com/webviewer/demo/doc/PDFTron.WebViewer.html'>API</Link></p>
+        <p><Link to='/webviewer/changelog'>Changelog</Link></p>
+      </div>
+      <div className='col'>
+        <h1><Link to='/android'>Android</Link></h1>
+        <p><Link to='/android/guides'>Guides</Link></p>
+        <p><Link to='https://www.pdftron.com/pdfnet/mobile/docs/Android/index.html'>API</Link></p>
+      </div>
+      <div className='col'>
+        <h1><Link to='/ios'>iOS</Link></h1>
+        <p><Link to='/ios/guides'>Guides</Link></p>
+        <p><Link to='https://www.pdftron.com/pdfnet/mobile/docs/iOS/src/index.html'>API</Link></p>
+      </div>
+    </div>
+  </div>
+);
